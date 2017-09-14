e841e68f214ca9915532e3e49323ede4d0e2deee
nohack <admin@nuptzj.cn>
2017-09-14 15:44:37 +0800

    多线程扫描

--- scan/jdwp_verify.py
+++ scan/jdwp_verify.py
@@ -1,9 +1,13 @@
 import Queue
 import threading
 import time
+import socket
+import sys
 queue = Queue.Queue()
 locker = threading.Lock()
-Thread_num = 2
+Thread_num = 20
+HANDSHAKE = "JDWP-Handshake"
+TIMEOUT = 5
 
 class ThreadNum(threading.Thread):
 	"""docstring for ThreadNum"""
@@ -17,7 +21,21 @@ class ThreadNum(threading.Thread):
 				current_task = self.queue.get()
 			except:
 				break
-			log(current_task)
+			host,port = current_task.split(":")
+			port = int(port)
+			s = socket.socket()
+			try:
+				log('testing %s:%s ....' % (host,str(port)))
+				s.connect( (host, port) )
+				s.send( HANDSHAKE )
+				hand_msg = s.recv( len(HANDSHAKE) )
+				if hand_msg != HANDSHAKE:
+					msg = 'HOST: %s:%s hand failed , hand msg %s' % (host,port,hand_msg)
+				else:
+					msg = 'HOST: %s:%s hand success!' % (host,port)
+				log(msg)
+			except:
+			    pass
 
 def add_task(tasks):
 	for i in range(0,len(tasks)):
@@ -30,12 +48,20 @@ def t_join(t_num):
 	while  True:
 		time.sleep(1)
 		ac_num = threading.activeCount()
+		log('active count: %s' % str(ac_num-1))
 		if ac_num <= 1 or queue.empty():
 			break
 
 if __name__ == '__main__':
-	tasks = ['1234','5678','233333','5555','QAQAQAQ']
+	# tasks = ['1234','5678','233333','5555','QAQAQAQ']
+	tasks =[]
+	ips = open('ip.txt','r').readlines()
+	for ip in ips:
+		host = ip.split(' ')[0]
+		port = ip.split(' ')[1].split('/')[0]
+		tasks.append(":".join([host,port]))
 	add_task(tasks)
+	socket.setdefaulttimeout(TIMEOUT)
 	for i in range(0,Thread_num):
 		t = ThreadNum(queue)
 		t.setDaemon(True)
