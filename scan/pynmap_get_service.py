7070fc71c1ddd98398d73fcb188e65a421ec3530
nohack <admin@nuptzj.cn>
2017-09-17 21:40:19 +0800

    测试。。

--- scan/pynmap_get_service.py
+++ scan/pynmap_get_service.py
@@ -5,7 +5,11 @@ from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser, NmapParserException
 import sys
 
-
+MAX_THREAD_NUM = 10 
+usage = """
+		masscan 192.168.1.0/24 -p 1-65535 >out
+		python %s out
+		""" % sys.argv[0]
 # start a new nmap scan on localhost with some specific options
 def do_scan(targets, options):
     parsed = None
@@ -13,7 +17,7 @@ def do_scan(targets, options):
     rc = nmproc.run()
     if rc != 0:
         print("nmap scan failed: {0}".format(nmproc.stderr))
-    print(type(nmproc.stdout))
+    # print(nmproc.stdout)
 
     try:
         parsed = NmapParser.parse(nmproc.stdout)
@@ -25,35 +29,25 @@ def do_scan(targets, options):
 
 # print scan results from a nmap report
 def print_scan(nmap_report):
-    print("Starting Nmap {0} ( http://nmap.org ) at {1}".format(
-        nmap_report.version,
-        nmap_report.started))
-    content = ""
+    # print("Starting Nmap {0} ( http://nmap.org ) at {1}".format(
+    #     nmap_report.version,
+    #     nmap_report.started))
     for host in nmap_report.hosts:
         if len(host.hostnames):
             tmp_host = host.hostnames.pop()
         else:
             tmp_host = host.address
 
-        print("Nmap scan report for {0} ({1})".format(
-            tmp_host,
-            host.address))
-        print("Host is {0}.".format(host.status))
-        print("  PORT     STATE         SERVICE")
-
         for serv in host.services:
-            pserv = "{0:>5s}/{1:3s}  {2:12s}  {3}".format(
+            pserv = "{0}  {1:>3s}/{2:3s}  {3:12s}  {4}".format(tmp_host,
                     str(serv.port),
                     serv.protocol,
                     serv.state,
                     serv.service)
-            if (serv.port == int(scan_port)) and (serv.state == "open"):
-                content = content + tmp_host + "\n"
             if len(serv.banner):
                 pserv += " ({0})".format(serv.banner)
             print(pserv)
-    write_to_file('out.txt',content)
-    print(nmap_report.summary)
+	# print(nmap_report.summary)
 
 def write_to_file(filename,content):
     outfile = open(filename,'w');
@@ -62,14 +56,18 @@ def write_to_file(filename,content):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit('usage: python %s ip -p{port}' % sys.argv[0])
-    ips = sys.argv[1]
-    opts = "-sV %s" % sys.argv[2]
-    _,scan_port = sys.argv[2].split('p')
-    # print scan_port
-    report = do_scan(ips, opts)
-    if report:
-        print_scan(report)
-    else:
-        print("No results returned")
+    if len(sys.argv) != 2:
+        sys.exit(usage)
+    masscan_results = open(sys.argv[1],'r').readlines()
+    print("   HOST          PORT   STATE         SERVICE")
+    for one_result in masscan_results:
+    	one_result = one_result.replace("\n","")
+    	if len(one_result.split(" "))>4:
+			ip = one_result.split(" ")[5]
+			port = one_result.split(" ")[3].split("/")[0]
+			opts = "-sV -p%s -Pn" % port
+			report = do_scan(ip, opts)
+			if report:
+				print_scan(report)
+			else:
+				print("No results returned")
