ea80694c67c0b06a35163396f5d0628ca3af83f3
nohack <admin@nuptzj.cn>
2017-09-17 20:03:43 +0800

    多线程 不阻塞

--- burp_sqli_ext/fserver.py
+++ burp_sqli_ext/fserver.py
@@ -1,12 +1,42 @@
 from flask import Flask
 import json
 from flask import request
+import Queue
+import threading
+from isqlmap import isqlmap
+import time
+queue = Queue.Queue()
+locker = threading.Lock()
+Thread_num = 5
 app = Flask(__name__)
+isqlmap = isqlmap()
+class ThreadNum(threading.Thread):
+	"""docstring for ThreadNum"""
+	def __init__(self, queue):
+		threading.Thread.__init__(self)
+		self.queue = queue
 
+	def run(self):
+		while True:
+			try:
+				if queue.empty():
+					time.sleep(1)
+					continue
+				current_task = self.queue.get()
+			except:
+				time.sleep(1)
+				continue
+			isqlmap.extract_request(current_task[0],current_task[1],current_task[2],current_task[3])
+			log(current_task[0])
+
+def add_task(task):
+	queue.put(task)
+def log(info):
+	locker.acquire()
+	print info
+	locker.release()
 @app.route('/api',methods=["POST"])
 def hello_world():
-	from isqlmap import isqlmap
-	isqlmap = isqlmap()
 	uri = request.form['url']
 	out = open('out_url.txt','a')
 	out.write(uri+"\n")
@@ -18,8 +48,12 @@ def hello_world():
 	#print headers
 	body = request.form['body']
 	#print body
-	isqlmap.extract_request(uri,method,headers,body)
+	add_task([uri,method,headers,body])
  	return 'ok'
-
 if __name__ == '__main__':
-    app.run(port=8989)
+	# tasks = ['1234','5678','233333','5555','QAQAQAQ']
+	for i in range(0,Thread_num):
+		t = ThreadNum(queue)
+		t.setDaemon(True)
+		t.start()
+	app.run(port=8989)
